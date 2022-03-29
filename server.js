2bbb2dc39ebb1e455216fa10c465b219673ffce9
Suppawich Tawornpichayachai <upsaga77@hotmail.com>
2022-03-29 23:26:49 +0700

    Add security to my API

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -11,11 +17,21 @@ connectDB();
 const hospitals = require("./routes/hospitals");
 const auth = require("./routes/auth");
 const appointments = require("./routes/appointments");
+const limiter = rateLimit({
+  windowsMs: 10 * 60 * 1000, //10 mins
+  max: 100,
+});
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+app.use(limiter);
+app.use(hpp());
+app.use(cors());
 
 app.use("/api/v1/hospitals", hospitals);
 app.use("/api/v1/auth", auth);
